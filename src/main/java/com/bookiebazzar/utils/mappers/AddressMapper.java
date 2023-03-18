d6d5b924dbd81b7dc13b3e7de269c01bb57e4d11
Ahmed Yassin <yassinahmed670@gmail.com>
2023-03-18 02:10:07 +0200

    address mapper

---
+++ src/main/java/com/bookiebazzar/utils/mappers/AddressMapper.java
@@ -0,0 +1,28 @@
+package com.bookiebazzar.utils.mappers;
+
+import com.bookiebazzar.model.dtos.AddressDto;
+import com.bookiebazzar.model.entities.Address;
+
+public class AddressMapper {
+
+    public static AddressDto toDto(Address address) {
+        AddressDto addressDto = new AddressDto();
+        addressDto.setId(address.getId());
+        addressDto.setCountry(address.getCountry());
+        addressDto.setCity(address.getCity());
+        addressDto.setStreet(address.getStreet());
+        addressDto.setBuildingNumber(address.getBuildingNumber());
+        return addressDto;
+    }
+
+    public static Address toEntity(AddressDto addressDto) {
+        Address address = new Address();
+        address.setId(addressDto.getId());
+        address.setCountry(addressDto.getCountry());
+        address.setCity(addressDto.getCity());
+        address.setStreet(addressDto.getStreet());
+        address.setBuildingNumber(addressDto.getBuildingNumber());
+        return address;
+
+    }
+}
